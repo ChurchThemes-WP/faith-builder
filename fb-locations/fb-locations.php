b33517e5224dcbad7f3b10aeb46e30ea3f8cb748
Anthony Garand <garand@garand.me>
2015-06-19 19:26:28 -0400

    Add locations module

---
+++ fb-locations/fb-locations.php
@@ -0,0 +1,125 @@
+<?php
+
+class FBLocations extends FLBuilderModule {
+
+    public function __construct()
+    {
+        parent::__construct(array(
+            'name'          => __( 'Locations', 'fl-builder' ),
+            'description'   => __( 'ChurchThemes Locations', 'fl-builder' ),
+            'category'      => __( FB_MODULES_SECTION, 'fl-builder' ),
+            'dir'           => FB_MODULES_DIR . 'fb-locations/',
+            'url'           => FB_MODULES_URL . 'fb-locations/',
+            'editor_export' => true,
+            'enabled'       => true,
+        ));
+
+        $this->add_css('fb-locations', $this->url . 'includes/style.css');
+    }
+}
+
+FLBuilder::register_module( 'FBLocations', array(
+    'general'      => array(
+        'title'         => __( 'General', 'fl-builder' ),
+        'sections'      => array(
+            'fb-locations'  => array(
+                'fields'        => array(
+                    'title'     => array(
+                        'type'          => 'text',
+                        'label'         => __( 'Title', 'fl-builder' ),
+                        'default'       => __( 'Locations', 'fl-builder' ),
+                    ),
+                    'layout' => array(
+                        'type'          => 'select',
+                        'label'         => __( 'Layout', 'fl-builder' ),
+                        'default'       => 'list',
+                        'options'       => array(
+                            'list'      => __( 'List', 'fl-builder' ),
+                            'grid'      => __( 'Grid', 'fl-builder' )
+                        )
+                    ),
+                    'orderby' => array(
+                        'type'          => 'select',
+                        'label'         => __( 'Order By', 'fl-builder' ),
+                        'default'       => 'custom-order',
+                        'options'       => array(
+                            'custom-order' => __( 'Custom Order', 'fl-builder' ),
+                            'name'         => __( 'Name', 'fl-builder' )
+                        )
+                    ),
+                    'order' => array(
+                        'type'          => 'select',
+                        'label'         => __( 'Sort', 'fl-builder' ),
+                        'default'       => 'high-to-low',
+                        'options'       => array(
+                            'low-to-high'  => __( 'Low to High', 'fl-builder' ),
+                            'high-to-low'  => __( 'High to Low', 'fl-builder' )
+                        )
+                    ),
+                    'limit' => array(
+                        'type'          => 'select',
+                        'label'         => __( 'Limit', 'fl-builder' ),
+                        'default'       => '5',
+                        'options'       => array(
+                            '1'      => __( '1', 'fl-builder' ),
+                            '2'      => __( '2', 'fl-builder' ),
+                            '3'      => __( '3', 'fl-builder' ),
+                            '4'      => __( '4', 'fl-builder' ),
+                            '5'      => __( '5', 'fl-builder' ),
+                            '6'      => __( '6', 'fl-builder' ),
+                            '7'      => __( '7', 'fl-builder' ),
+                            '8'      => __( '8', 'fl-builder' ),
+                            '9'      => __( '9', 'fl-builder' ),
+                            '10'      => __( '10', 'fl-builder' )
+                        )
+                    ),
+                    'show_image' => array(
+                        'type'          => 'select',
+                        'label'         => __( 'Show image', 'fl-builder' ),
+                        'default'       => '1',
+                        'options'       => array(
+                            '1'      => __( 'Yes', 'fl-builder' ),
+                            '0'      => __( 'No', 'fl-builder' )
+                        )
+                    ),
+                    'show_address' => array(
+                        'type'          => 'select',
+                        'label'         => __( 'Show address', 'fl-builder' ),
+                        'default'       => '1',
+                        'options'       => array(
+                            '1'      => __( 'Yes', 'fl-builder' ),
+                            '0'      => __( 'No', 'fl-builder' )
+                        )
+                    ),
+                    'show_phone' => array(
+                        'type'          => 'select',
+                        'label'         => __( 'Show phone', 'fl-builder' ),
+                        'default'       => '0',
+                        'options'       => array(
+                            '1'      => __( 'Yes', 'fl-builder' ),
+                            '0'      => __( 'No', 'fl-builder' )
+                        )
+                    ),
+                    'show_times' => array(
+                        'type'          => 'select',
+                        'label'         => __( 'Show times', 'fl-builder' ),
+                        'default'       => '0',
+                        'options'       => array(
+                            '1'      => __( 'Yes', 'fl-builder' ),
+                            '0'      => __( 'No', 'fl-builder' )
+                        )
+                    ),
+                    'show_map' => array(
+                        'type'          => 'select',
+                        'label'         => __( 'Show map', 'fl-builder' ),
+                        'default'       => '0',
+                        'options'       => array(
+                            '1'      => __( 'Yes', 'fl-builder' ),
+                            '0'      => __( 'No', 'fl-builder' )
+                        )
+                    )
+                )
+            )
+        )
+    )
+));
---
+++ fb-locations/includes/frontend.php
@@ -0,0 +1,84 @@
+<div class="fb-locations fb-locations--layout-<?php echo $settings->layout; ?>">
+  <?php
+
+    $instance['title'] = $settings->title;
+    $instance['orderby'] = $settings->orderby;
+    $instance['order'] = $settings->order;
+    $instance['limit'] = $settings->limit;
+    $instance['show_image'] = $settings->show_image;
+    $instance['show_address'] = $settings->show_address;
+    $instance['show_phone'] = $settings->show_phone;
+    $instance['show_times'] = $settings->show_times;
+    $instance['show_map'] = $settings->show_map;
+
+    global $post;
+
+    if ($settings->title) { ?>
+      <h1 class="fb-locations-title"><?php echo $settings->title; ?></h1>
+    <?php }
+
+    // Get posts
+    $posts = ctc_get_locations( $instance );
+
+    // Loop Posts
+    $i = 0;
+    foreach ( $posts as $post ) : setup_postdata( $post ); $i++;
+
+      $ctc_location_address = ctc_location_data()['address'];
+      $ctc_location_show_directions_link = ctc_location_data()['show_directions_link'];
+      $ctc_location_phone = ctc_location_data()['phone'];
+      $ctc_location_times = ctc_location_data()['times'];
+      $ctc_location_directions_url = ctc_location_data()['directions_url'];
+
+      ?>
+
+      <div class="fb-locations--location">
+        <?php if ( !$instance['show_image'] && $instance['show_map'] ) : ?>
+          <div class="fb-locations--location-map">
+            <iframe class="fb-locations--location-map-embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.it/maps?q=<?php echo urlencode( $ctc_location_address ); ?>&output=embed"></iframe>
+          </div>
+        <?php elseif ( $instance['show_image'] && has_post_thumbnail() ) : ?>
+          <a class="fb-locations--location-image" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+            <?php the_post_thumbnail( 'ctc-thumb-small' ); ?>
+          </a>
+        <?php endif; ?>
+
+        <h2 class="fb-locations--location-name">
+          <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+        </h2>
+
+        <div class="fb-locations--location-meta">
+          <?php if ( $instance['show_address'] && $ctc_location_address ) : ?>
+          <div class="fb-locations--location-address">
+            <i class="genericon genericon-location"></i>
+            <?php echo $ctc_location_address; ?>
+          </div>
+          <?php endif; ?>
+
+          <?php if ( $instance['show_phone'] && $ctc_location_phone ) : ?>
+          <div class="fb-locations--location-phone">
+            <i class="genericon genericon-phone"></i>
+            <?php echo $ctc_location_phone; ?>
+          </div>
+          <?php endif; ?>
+
+          <?php if ( $instance['show_times'] && $ctc_location_times ) : ?>
+          <a href="mailto:<?php echo $ctc_location_times; ?>" class="fb-locations--location-times">
+            <i class="genericon genericon-time"></i>
+            <?php echo $ctc_location_times; ?>
+          </a>
+          <?php endif; ?>
+        </div>
+
+        <?php if ( $instance['show_address'] && $ctc_location_directions_url ) : ?>
+          <a href="<?php echo $ctc_location_directions_url; ?>" class="button">Get Directions</a>
+        <?php endif; ?>
+
+      </div>
+
+      <?php
+
+    endforeach;
+
+  ?>
+</div>
